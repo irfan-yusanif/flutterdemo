5e8121b6fbeaed3f8da00bc1fedcdd0058d42f90
Irfan <m.irfanwatoo@gmail.com>
2025-02-22 12:59:46 +0000

    show create user in user management in settings

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e960a60d9fcec5319c9f5e7f6cf368cb",
+const RESOURCES = {"flutter_bootstrap.js": "6844aea663804bab1c2082b0c70b0a6b",
 "version.json": "404ac7ee9da5da8572940346f863bd6d",
 "index.html": "7473fe410888d459242521920ebe8655",
 "/": "7473fe410888d459242521920ebe8655",
-"main.dart.js": "d0c4416468359259c0180e82b35fec3b",
+"main.dart.js": "701773dd8140d218cbce39ae47751814",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "4a5f397e8ae1a2bce4592972b26a883f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
